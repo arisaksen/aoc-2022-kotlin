309f5a2619cf33e1a880bdf8e0dd6d39da1433ee
Arne Ruben Isaksen <arne.isaksen@embriq.no>
2022-12-04 07:26:04 +0100

    Day04 part1

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,11 +1,15 @@
 import org.assertj.core.api.Assertions.assertThat
 
+
 fun main() {
 
-    fun part1(items: List<String>): Int {
-
-        return 1
-    }
+    fun part1(items: List<String>): Int =
+        items
+            .map { it.split(",") }
+            .map { it.map { range -> (range.substringBefore("-").toInt()..range.substringAfter("-").toInt()) } }
+            .also { it.map { println(it) } }
+            .filter { it.first().checkIfIntRangeContains(it.last()) }
+            .size
 
     fun part2(items: List<String>): Int {
 
@@ -14,10 +18,17 @@ fun main() {
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    assertThat(part1(testInput)).isEqualTo(0)
-    assertThat(part2(testInput)).isEqualTo(0)
+    assertThat(part1(testInput)).isEqualTo(2)
+//    assertThat(part2(testInput)).isEqualTo(0)
 
     val input = readInput("Day04")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
+
+fun IntRange.checkIfIntRangeContains(range: IntRange): Boolean =
+    when ((this intersect range).size) {
+        range.count() -> true
+        this.count() -> true
+        else -> false
+    }
