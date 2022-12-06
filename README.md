51bdfb3b496e97e7fc3def56a907dd62f70a611b
Arne Ruben Isaksen <arne.isaksen@embriq.no>
2022-12-06 07:47:55 +0100

    Day06 solved

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Day 3](https://img.shields.io/badge/Day--3-solved-green)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day03.kt)
 [![Day 4](https://img.shields.io/badge/Day--4-solved-green)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day04.kt)
 [![Day 5](https://img.shields.io/badge/Day--5-solved-green)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day05.kt)
-[![Day 6](https://img.shields.io/badge/Day--6-todo-yellow)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day06.kt)
+[![Day 6](https://img.shields.io/badge/Day--6-solved-green)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day06.kt)
 [![Day 7](https://img.shields.io/badge/Day--7-todo-yellow)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day07.kt)
 [![Day 8](https://img.shields.io/badge/Day--8-todo-yellow)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day08.kt)
 [![Day 9](https://img.shields.io/badge/Day--9-todo-yellow)](https://github.com/arisaksen/aoc-2022-kotlin/blob/main/src/Day09.kt)
--- src/Day06.kt
+++ src/Day06.kt
@@ -1,25 +1,54 @@
 import org.assertj.core.api.Assertions.assertThat
+import java.util.LinkedList
 
 fun main() {
+    val moreThanTwoEqual = """^.*(.).*\1.*${'$'}""".toRegex()
 
-    fun part1(input: String): String {
+    fun part1(input: String): Int {
+        val limitedQue = LinkedList<Char>()
+        val queSize = 4
 
-        return "1"
+        input.forEachIndexed { index, char ->
+            limitedQue.addNew(char, queSize)
+            if (limitedQue.size == queSize && limitedQue.joinToString("").contains(moreThanTwoEqual).not()) {
+                println("true after: ${limitedQue.joinToString("")}($index)")
+                return index + 1
+            }
+        }
+
+        return 0
     }
 
-    fun part2(input: String): String {
+    fun part2(input: String): Int {
+        val limitedQue = LinkedList<Char>()
+        val queSize = 14
 
-        return "1"
+        input.forEachIndexed { index, char ->
+            limitedQue.addNew(char, queSize)
+            if (limitedQue.size == queSize && limitedQue.joinToString("").contains(moreThanTwoEqual).not()) {
+                println("true after: ${limitedQue.joinToString("")}($index)")
+                return index + 1
+            }
+        }
+
+        return 0
     }
 
 
 // test if implementation meets criteria from the description, like:
     val testInput = readText("Day06_test")
-    assertThat(part1(testInput)).isEqualTo("CMZ")
-    assertThat(part2(testInput)).isEqualTo("MCD")
+    assertThat(part1(testInput)).isEqualTo(10)
+    assertThat(part2(testInput)).isEqualTo(29)
 
 // print the puzzle answer
     val input = readText("Day06")
     println(part1(input))
     println(part2(input))
+}
+
+fun LinkedList<Char>.addNew(char: Char, max: Int) {
+    this.add(char)
+    if (this.size > max) {
+        this.removeFirst()
+    }
 }
